54b03514675dcd083ba8a904860ce31bddc1ac18
alanb <alan.ballanger@live.fr>
2026-06-26 10:48:55 +0200

    move HUD of varyon UI

--- mods/varyon-UI/src/main/java/com/varyon/varyonui/hud/VaryonMenuHud.java
+++ mods/varyon-UI/src/main/java/com/varyon/varyonui/hud/VaryonMenuHud.java
@@ -23,26 +23,24 @@ public class VaryonMenuHud extends CustomUIHud {
 
     private static final int PNG_W = 189;
     private static final int PNG_H = 322;
-    private static final int ANCHOR_RIGHT = 12;
-    private static final int ANCHOR_BOTTOM = 20;
-    private static final int MENU_LIFT_PX = 70;
-    private static final int TITLE_SHIFT_RIGHT_PX = 7;
-    private static final int TITLE_NUDGE_DOWN_PX = 15;
-    private static final int HUD_SHIFT_DOWN_PX = 50;
+
+    private static final int ANCHOR_RIGHT = 392;
+    private static final int ANCHOR_TOP = 907;
     private static final int ANCHOR_WIDTH = 128;
 
-    private static final int IMG_W = 118;
+    private static final int DIVIDER_W = 4;
+    private static final int DIVIDER_H = 72;
+    private static final int DIVIDER_RIGHT = ANCHOR_RIGHT + ANCHOR_WIDTH - 20;
+    private static final int DIVIDER_TOP = 969;
+
+private static final int IMG_W = 118;
     private static final int IMG_H = (int) Math.round((double) IMG_W * (double) PNG_H / (double) PNG_W);
+
     private static final int KEY_H = 32;
     private static final int KEY_SRC_X = 109;
     private static final int KEY_SRC_Y = 224;
     private static final int KEY_LABEL_W_O = 32;
     private static final int KEY_LABEL_W_ALT = 38;
-    private static final int KEY_NUDGE_DOWN_PX = 1;
-
-    private static final int MENU_UP_PX = 56;
-
-    private static final int ANCHOR_HEIGHT = 24 + 28 + IMG_H + MENU_LIFT_PX;
 
     public static void attach(@Nullable Player player, @Nonnull PlayerRef playerRef) {
         if (player == null) {
@@ -76,48 +74,33 @@ public class VaryonMenuHud extends CustomUIHud {
             LOGGER.log(Level.WARNING, "Failed to build Varyon menu HUD", e);
             return;
         }
-        int menuOffsetY = -MENU_LIFT_PX + HUD_SHIFT_DOWN_PX;
 
         Anchor root = new Anchor();
         root.setRight(Value.of(ANCHOR_RIGHT));
-        root.setBottom(Value.of(ANCHOR_BOTTOM));
+        root.setTop(Value.of(ANCHOR_TOP));
         root.setWidth(Value.of(ANCHOR_WIDTH));
-        root.setHeight(Value.of(ANCHOR_HEIGHT));
+        root.setHeight(Value.of(IMG_H));
         builder.setObject("#VaryonMenuHud.Anchor", root);
-        builder.set("#VaryonMenuTitle.Text", "Menu Varyon");
-        UUID uuid = hudPlayer.getUuid();
-        AccueilShortcutConfig.Mode mode = AccueilShortcutConfig.getInstance().getMode(uuid);
-        String keyText = "O";
-        int keyLabelW = KEY_LABEL_W_O;
-        switch (mode) {
-            case ALT -> {
-                keyText = "Alt";
-                keyLabelW = KEY_LABEL_W_ALT;
-            }
-            case O -> {
-                keyText = "O";
-                keyLabelW = KEY_LABEL_W_O;
-            }
-            case DISABLE -> {
-                keyText = "O";
-                keyLabelW = KEY_LABEL_W_O;
-            }
-        }
-        builder.set("#VaryonMenuKey.Text", keyText);
 
-        Anchor title = new Anchor();
-        title.setLeft(Value.of(TITLE_SHIFT_RIGHT_PX));
-        title.setTop(Value.of(menuOffsetY + TITLE_NUDGE_DOWN_PX));
-        title.setWidth(Value.of(ANCHOR_WIDTH - TITLE_SHIFT_RIGHT_PX));
-        title.setHeight(Value.of(24));
-        builder.setObject("#VaryonMenuTitle.Anchor", title);
+        Anchor divider = new Anchor();
+        divider.setRight(Value.of(DIVIDER_RIGHT));
+        divider.setTop(Value.of(DIVIDER_TOP));
+        divider.setWidth(Value.of(DIVIDER_W));
+        divider.setHeight(Value.of(DIVIDER_H));
+        builder.setObject("#VaryonDividerGroup.Anchor", divider);
+
+        Anchor dividerImg = new Anchor();
+        dividerImg.setLeft(Value.of(0));
+        dividerImg.setTop(Value.of(0));
+        dividerImg.setWidth(Value.of(DIVIDER_W));
+        dividerImg.setHeight(Value.of(DIVIDER_H));
+        builder.setObject("#VaryonHotbarDivider.Anchor", dividerImg);
 
         int stackLeft = (ANCHOR_WIDTH - IMG_W) / 2;
-        int stackTop = 28 + menuOffsetY - MENU_UP_PX;
 
         Anchor stack = new Anchor();
         stack.setLeft(Value.of(stackLeft));
-        stack.setTop(Value.of(stackTop));
+        stack.setTop(Value.of(0));
         stack.setWidth(Value.of(IMG_W));
         stack.setHeight(Value.of(IMG_H));
         builder.setObject("#VaryonMenuStack.Anchor", stack);
@@ -136,20 +119,24 @@ public class VaryonMenuHud extends CustomUIHud {
         book.setHeight(Value.of(IMG_H));
         builder.setObject("#VaryonMenuBook.Anchor", book);
 
+        UUID uuid = hudPlayer.getUuid();
+        AccueilShortcutConfig.Mode mode = AccueilShortcutConfig.getInstance().getMode(uuid);
+        String keyText;
+        int keyLabelW;
+        if (mode == AccueilShortcutConfig.Mode.ALT) {
+            keyText = "Alt";
+            keyLabelW = KEY_LABEL_W_ALT;
+        } else {
+            keyText = "O";
+            keyLabelW = KEY_LABEL_W_O;
+        }
+        builder.set("#VaryonMenuKey.Text", keyText);
+
         int keyCx = (int) Math.round((double) KEY_SRC_X * (double) IMG_W / (double) PNG_W);
-        int keyCy = (int) Math.round((double) KEY_SRC_Y * (double) IMG_H / (double) PNG_H) + KEY_NUDGE_DOWN_PX;
-        int keyLeft = keyCx - keyLabelW / 2;
-        int keyTop = keyCy - KEY_H / 2;
-        if (keyLeft < 0) {
-            keyLeft = 0;
-        } else if (keyLeft > IMG_W - keyLabelW) {
-            keyLeft = IMG_W - keyLabelW;
-        }
-        if (keyTop < 0) {
-            keyTop = 0;
-        } else if (keyTop > IMG_H - KEY_H) {
-            keyTop = IMG_H - KEY_H;
-        }
+        int keyCy = (int) Math.round((double) KEY_SRC_Y * (double) IMG_H / (double) PNG_H);
+        int keyLeft = Math.max(0, Math.min(keyCx - keyLabelW / 2, IMG_W - keyLabelW));
+        int keyTop = Math.max(0, Math.min(keyCy - KEY_H / 2, IMG_H - KEY_H));
+
         Anchor key = new Anchor();
         key.setLeft(Value.of(keyLeft));
         key.setTop(Value.of(keyTop));
